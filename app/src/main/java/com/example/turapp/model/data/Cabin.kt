3c32f8f2cc132e97619619986b42a191ca606bb0
lenamarsilius <lena.j@live.dk>
2022-04-06 15:52:10 +0200

    Added ChooserMapFragment and ChooserMapFragmentViewModel

--- app/src/main/java/com/example/turapp/model/data/Cabin.kt
+++ app/src/main/java/com/example/turapp/model/data/Cabin.kt
@@ -3,5 +3,4 @@ package com.example.turapp.model.data
 data class Cabin(val id: Number?, val name: String?, val DDLat: Double, val DDLon: Double,
                  val air_temperature: Number?, val wind_speed: Number?, val precipitation_amount: String?)
 
-data class CabinList(val cabins: MutableList<Cabin>)
 
