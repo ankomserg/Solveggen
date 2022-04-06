65e68278a4667ef6e41356795b96dea1d8eec969
sondm <Tumejorpesadillacharmander95!>
2022-04-06 10:48:13 +0200

    Updated WeatherApi with input parameters

---
+++ app/src/main/java/com/example/turapp/model/interfaces/WeatherApi.kt
@@ -0,0 +1,15 @@
+package com.example.turapp.model.interfaces
+
+import com.example.turapp.model.data.Weather
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+    @GET("/weatherapi/locationforecast/2.0/compact")
+    suspend fun getWeather(
+        @Query("lat") lat: Number?,
+        @Query("lon") lon: Number?)
+            : Response<Weather>
+}
+
