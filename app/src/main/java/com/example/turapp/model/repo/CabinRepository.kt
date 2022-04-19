1bfb5f401499501ce638289614b2f0b48db8c834
sondm <Tumejorpesadillacharmander95!>
2022-04-19 10:38:23 +0200

    Added loadWeather-method in repository class

--- app/src/main/java/com/example/turapp/model/repo/CabinRepository.kt
+++ app/src/main/java/com/example/turapp/model/repo/CabinRepository.kt
@@ -3,8 +3,11 @@ package com.example.turapp.model.repo
 import androidx.lifecycle.LiveData
 import com.example.turapp.model.data.Cabin
 import com.example.turapp.model.data.DataSource
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import com.example.turapp.model.data.Weather
+import com.example.turapp.model.interfaces.RetrofitHelper
+import com.example.turapp.model.interfaces.WeatherApi
+import kotlinx.coroutines.*
+import retrofit2.Response
 
 class CabinRepository(private val database: CabinRoomDatabase) {
     suspend fun loadCabins() : List<Cabin> {
@@ -20,12 +23,29 @@ class CabinRepository(private val database: CabinRoomDatabase) {
        return cabins
    }
 
-    suspend fun loadWeather() {
-        withContext(Dispatchers.IO) {
-            val cabins = getCabins()
-
+    suspend fun loadWeather(): List<Cabin> {
+        val dataApi = RetrofitHelper.getInstance().create(WeatherApi::class.java)
+        val cabins = getCabins()
+        val allWeather = mutableListOf<Deferred<Weather?>>()
+        for (cabin in cabins) {
+            val result = CoroutineScope(Dispatchers.IO).async {
+                dataApi.getWeather(cabin.DDLat, cabin.DDLon).body()
+                }
+            allWeather.add(result)
         }
+        val readyWeather = allWeather.awaitAll()
+        for (cabin in cabins) {
+            for (weather in readyWeather) {
+                if (weather != null) {
+                    if (cabin.DDLat == weather.geometry?.coordinates?.get(0)
+                        && cabin.DDLon == weather.geometry?.coordinates?.get(1)) {
 
+                    }
+
+                }
+            }
+        }
+        return cabins
     }
 
     private suspend fun deleteAll() {
@@ -59,3 +79,4 @@ class CabinRepository(private val database: CabinRoomDatabase) {
 
     }
 }
+
