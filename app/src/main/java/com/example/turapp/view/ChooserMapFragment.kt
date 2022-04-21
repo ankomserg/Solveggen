04414ee1edbbea40f937e472cba5d4bc8b002cc7
lenamarsilius <lena.j@live.dk>
2022-04-21 12:35:16 +0200

    Added shortcut button from map to list

--- app/src/main/java/com/example/turapp/view/ChooserMapFragment.kt
+++ app/src/main/java/com/example/turapp/view/ChooserMapFragment.kt
@@ -6,55 +6,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import com.example.turapp.databinding.FragmentChooserMapBinding
 import com.example.turapp.viewmodel.ChooserMapFragmentViewModel
 import com.google.android.gms.maps.model.CameraPosition
 
 
 class ChooserMapFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
+    private var _binding : FragmentChooserMapBinding? = null
 
-    }
+    private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment_chooser_map, container, false)
+        _binding = FragmentChooserMapBinding.inflate(inflater, container, false)
+        return binding.root
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val viewModel: ChooserMapFragmentViewModel by viewModels()
         viewModel.loadCabins()
 
         val mapFragment = childFragmentManager
             .findFragmentById(R.id.map_view) as SupportMapFragment?
-        mapFragment!!.getMapAsync{ mMap ->
+        mapFragment!!.getMapAsync { mMap ->
             mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            mMap.uiSettings.isZoomControlsEnabled = true
             mMap.clear()
-            val googlePlex = CameraPosition.builder()
+            val cameraPosition = CameraPosition.builder()
                 .target(LatLng(59.94, 10.72))
                 .zoom(5f)
                 .build()
 
-            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null)
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
 
             viewModel.getCabins().observe(viewLifecycleOwner, Observer {
                 it.forEach { Cabin ->
                     val cPos = LatLng(Cabin.DDLat!!, Cabin.DDLon!!)
-                    mMap.addMarker(MarkerOptions().position(cPos).title("Cabin").snippet(Cabin.name.toString()))
+                    mMap.addMarker(
+                        MarkerOptions().position(cPos).title("Cabin").snippet(Cabin.name.toString())
+                    )
                 }
             })
         }
-        return rootView
 
+        binding.startActivityButton.setOnClickListener {
+            it.findNavController().navigate(R
+                .id.action_chooserMapFragment_to_chooseListFragment2)
+        }
     }
 
 }
