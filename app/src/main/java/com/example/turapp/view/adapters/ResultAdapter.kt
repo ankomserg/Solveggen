c5bfcfbadb5c75ab05b4399355c9b13db0fcd8ea
sondm <Tumejorpesadillacharmander95!>
2022-05-05 17:17:35 +0200

    Started to implement new method for choosing dates

--- app/src/main/java/com/example/turapp/view/adapters/ResultAdapter.kt
+++ app/src/main/java/com/example/turapp/view/adapters/ResultAdapter.kt
@@ -20,12 +20,13 @@ class ResultAdapter(private val cabins: MutableList<Cabin>
         val firstInfo: TextView = view.findViewById(R.id.info_first)
         val secondInfo: TextView = view.findViewById(R.id.info_second)
         val cabinPhoto: ImageFilterView = view.findViewById(R.id.cabin_picture)
-//        val weatherPhoto: ImageFilterView = view.findViewById(R.id.weather_picture)
-//        val temperature: TextView = view.findViewById(R.id.weather_forecast_temperature)
-//        val rain: TextView = view.findViewById(R.id.weather_forecast_rain)
-//        val wind: TextView = view.findViewById(R.id.weather_forecast_wind)
+        val weatherPhoto: ImageFilterView = view.findViewById(R.id.weather_picture)
+        val temperature: TextView = view.findViewById(R.id.weather_forecast_temperature)
+        val rain: TextView = view.findViewById(R.id.weather_forecast_rain)
+        val wind: TextView = view.findViewById(R.id.weather_forecast_wind)
         val chooser: CheckBox = view.findViewById(R.id.choose_list_checkbox)
         val moreInfo: Button = view.findViewById(R.id.more_info)
+        val map: View = view.findViewById(R.id.map_holder)
 
     }
 
@@ -38,13 +39,14 @@ class ResultAdapter(private val cabins: MutableList<Cabin>
     override fun onBindViewHolder(holder: CabinViewHolder, position: Int) {
         val showedCabin = cabins[position]
         holder.title.text = showedCabin.name
-        holder.firstInfo.text = showedCabin.id.toString()
-        holder.secondInfo.text = showedCabin.DDLat.toString()
-//        holder.temperature.text = showedCabin.air_temperature.toString()
-//        holder.rain.text = showedCabin.precipitation_amount.toString()
-//        holder.wind.text = showedCabin.wind_speed.toString()
+        holder.firstInfo.visibility = View.GONE
+        holder.secondInfo.visibility = View.GONE
+        holder.temperature.text = showedCabin.air_temperature.toString()
+        holder.rain.text = showedCabin.precipitation_amount.toString()
+        holder.wind.text = showedCabin.wind_speed.toString()
         //holder.weatherPhoto.setAltImageResource(R.drawable.ic_baseline_add_a_photo_24)
         holder.chooser.visibility = View.GONE
+        holder.map.visibility = View.GONE
 
         holder.moreInfo.setOnClickListener {
             it.findNavController().navigate(
