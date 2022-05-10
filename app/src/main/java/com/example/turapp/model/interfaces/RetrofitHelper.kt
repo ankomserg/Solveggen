1caf75bd52631c2a0155fe229377445ea838354b
sondm <Tumejorpesadillacharmander95!>
2022-05-10 15:28:38 +0200

    Updated API call for Cabins with RetroFit

--- app/src/main/java/com/example/turapp/model/interfaces/RetrofitHelper.kt
+++ app/src/main/java/com/example/turapp/model/interfaces/RetrofitHelper.kt
@@ -5,9 +5,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
     val url = "https://in2000-apiproxy.ifi.uio.no/"
+    val test = "https://my-json-server.typicode.com/"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun getCabinIntance(): Retrofit {
+        return Retrofit.Builder().baseUrl(test)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
