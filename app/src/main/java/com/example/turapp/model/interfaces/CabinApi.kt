5bd2eff0adf2e3addb4cd3ff62bd5a3176dc077b
sondm <Tumejorpesadillacharmander95!>
2022-05-10 16:26:34 +0200

    Updated nav_graph and strings.night

--- app/src/main/java/com/example/turapp/model/interfaces/CabinApi.kt
+++ app/src/main/java/com/example/turapp/model/interfaces/CabinApi.kt
@@ -1,11 +1,9 @@
 package com.example.turapp.model.interfaces
 
 import com.example.turapp.model.data.Cabin
-import com.example.turapp.model.data.Weather
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface CabinApi {
     @GET("lenamarsilius/{number}/cabins")
