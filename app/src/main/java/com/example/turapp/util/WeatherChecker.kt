592cfcca57599d6845dc961610204e8127f02026
sondm <Tumejorpesadillacharmander95!>
2022-05-15 16:10:43 +0200

    Updated WeatherChecker.kt and WeatherCheckerTest.kt with correct algorithm

--- app/src/main/java/com/example/turapp/util/WeatherChecker.kt
+++ app/src/main/java/com/example/turapp/util/WeatherChecker.kt
@@ -1,5 +1,6 @@
 package com.example.turapp.util
 
+import android.util.Log
 import com.example.turapp.model.data.Cabin
 
 class WeatherChecker {
@@ -7,7 +8,6 @@ class WeatherChecker {
         fun sort(cabins : List<Cabin>, option : String) : List<Cabin> {
 
             val sortedCabins = mutableListOf<Cabin>()
-
             val map = HashMap<Double?, MutableList<Cabin>>()
             val elementList = ArrayList<Double>()
 
@@ -32,29 +32,50 @@ class WeatherChecker {
                             }
                         }
                     }
+                }
 
-//                    var i = 0
-//                    var temp = sortedCabins[i]
-//                    var next : Cabin
-//
-//                    while (i < sortedCabins.size - 1) {
-//                        next = sortedCabins[i + 1]
-//
-//                        if (temp.air_temperature == next.air_temperature) {
-//                            if (temp.precipitation_amount!! > next.precipitation_amount!!) {
-//                                sortedCabins[i] = next
-//                                sortedCabins[i + 1] = temp
-//                            } else if (temp.precipitation_amount!! == next.precipitation_amount!!) {
-//                                if (temp.wind_speed!! > next.wind_speed!!) {
-//                                    sortedCabins[i] = next
-//                                    sortedCabins[i + 1] = temp
-//                                }
-//                            }
-//                        }
-//
-//                        temp = sortedCabins[i + 1]
-//                        i++
-//                    }
+                "rain" -> {
+                    for (cabin in cabins) {
+                        if (!map.containsKey(cabin.precipitation_amount)) {
+                            map.put(cabin.precipitation_amount, mutableListOf<Cabin>())
+                            map[cabin.precipitation_amount]!!.add(cabin)
+                            elementList.add(cabin.precipitation_amount!!)
+                        } else {
+                            map[cabin.precipitation_amount]!!.add(cabin)
+                        }
+                    }
+
+                    for (element in elementList) {
+                        val newSortedCabins = map.get(element)?.let { secondarySort(it, option) }
+
+                        if (newSortedCabins != null) {
+                            for (sortedCabin in newSortedCabins) {
+                                sortedCabins.add(sortedCabin)
+                            }
+                        }
+                    }
+                }
+
+                "wind" -> {
+                    for (cabin in cabins) {
+                        if (!map.containsKey(cabin.wind_speed)) {
+                            map.put(cabin.wind_speed, mutableListOf<Cabin>())
+                            map[cabin.wind_speed]!!.add(cabin)
+                            elementList.add(cabin.wind_speed!!)
+                        } else {
+                            map[cabin.wind_speed]!!.add(cabin)
+                        }
+                    }
+
+                    for (element in elementList) {
+                        val newSortedCabins = map.get(element)?.let { secondarySort(it, option) }
+
+                        if (newSortedCabins != null) {
+                            for (sortedCabin in newSortedCabins) {
+                                sortedCabins.add(sortedCabin)
+                            }
+                        }
+                    }
                 }
             }
 
@@ -105,6 +126,43 @@ class WeatherChecker {
                         }
                     }
                 }
+
+                else -> {
+                    var swap = true
+                    while(swap){
+                        swap = false
+                        for(i in 0 until sortedCabins.indices.last){
+                            if(sortedCabins[i].air_temperature!! < sortedCabins[i + 1].air_temperature!!) {
+                                val temp = sortedCabins[i]
+                                sortedCabins[i] = sortedCabins[i+1]
+                                sortedCabins[i + 1] = temp
+
+                                swap = true
+                            }
+                        }
+                    }
+                    Log.d("HER", sortedCabins.toString())
+                    for (cabin in sortedCabins) {
+                        if (!map.containsKey(cabin.air_temperature)) {
+                            map.put(cabin.air_temperature, mutableListOf<Cabin>())
+                            map[cabin.air_temperature]!!.add(cabin)
+                            elementList.add(cabin.air_temperature!!)
+                        } else {
+                            map[cabin.air_temperature]!!.add(cabin)
+                        }
+                    }
+
+                    sortedCabins = mutableListOf()
+
+                    for (element in elementList) {
+                        val newSortedCabins = map.get(element)?.let { finalSort(it, option) }
+                        if (newSortedCabins != null) {
+                            for (sortedCabin in newSortedCabins) {
+                                sortedCabins.add(sortedCabin)
+                            }
+                        }
+                    }
+                }
             }
 
             return sortedCabins
@@ -129,7 +187,37 @@ class WeatherChecker {
                             }
                         }
                     }
+                }
 
+                "rain" -> {
+                    var swap = true
+                    while(swap){
+                        swap = false
+                        for(i in 0 until sortedCabins.indices.last){
+                            if(sortedCabins[i].wind_speed!! > sortedCabins[i+1].wind_speed!!){
+                                val temp = sortedCabins[i]
+                                sortedCabins[i] = sortedCabins[i+1]
+                                sortedCabins[i + 1] = temp
+
+                                swap = true
+                            }
+                        }
+                    }
+                }
+
+                else -> {
+                    var swap = true
+                    while(swap){
+                        swap = false
+                        for(i in 0 until sortedCabins.indices.last){
+                            if(sortedCabins[i].precipitation_amount!! > sortedCabins[i+1].precipitation_amount!!){
+                                val temp = sortedCabins[i]
+                                sortedCabins[i] = sortedCabins[i+1]
+                                sortedCabins[i + 1] = temp
+                                swap = true
+                            }
+                        }
+                    }
                 }
             }
 
--- app/src/test/java/com/example/turapp/util/WeatherCheckerTest.kt
+++ app/src/test/java/com/example/turapp/util/WeatherCheckerTest.kt
@@ -6,10 +6,10 @@ import org.junit.Test
 
 class WeatherCheckerTest {
     @Test
-    fun sortTest() {
+    fun sortTestEqualTemperature() {
         val cabin1 = Cabin(
             1, "Skihytta", null, null, 18.0,
-            10.0, 100.0, 18.0, 10.0,
+            9.0, 50.0, 18.0, 9.0,
             null, null, null, null, null,
             null, null, null, null, null,
             null
@@ -17,7 +17,7 @@ class WeatherCheckerTest {
 
         val cabin2 = Cabin(
             1, "Skihytta", null, null, 18.0,
-            10.0, 80.0, 18.0, 10.0,
+            5.0, 50.0, 18.0, 12.0,
             null, null, null, null, null,
             null, null, null, null, null,
             null
@@ -25,7 +25,7 @@ class WeatherCheckerTest {
 
         val cabin3 = Cabin(
             1, "Skihytta", null, null, 18.0,
-            10.0, 50.0, 18.0, 10.0,
+            10.0, 50.0, 18.0, 11.0,
             null, null, null, null, null,
             null, null, null, null, null,
             null
@@ -35,6 +35,72 @@ class WeatherCheckerTest {
 
         println(WeatherChecker.sort(cabins, "temperature"))
 
-        assertEquals(cabin3, WeatherChecker.sort(cabins, "temperature").get(0))
+        assertEquals(cabin2, WeatherChecker.sort(cabins, "temperature").get(0))
+    }
+
+    @Test
+    fun sortTestEqualRain() {
+        val cabin1 = Cabin(
+            1, "Skihytta", null, null, 17.0,
+            9.0, 50.0, 18.0, 9.0,
+            null, null, null, null, null,
+            null, null, null, null, null,
+            null
+        )
+
+        val cabin2 = Cabin(
+            1, "Skihytta", null, null, 18.0,
+            5.0, 50.0, 18.0, 12.0,
+            null, null, null, null, null,
+            null, null, null, null, null,
+            null
+        )
+
+        val cabin3 = Cabin(
+            1, "Skihytta", null, null, 18.0,
+            10.0, 50.0, 15.0, 11.0,
+            null, null, null, null, null,
+            null, null, null, null, null,
+            null
+        )
+
+        val cabins = listOf(cabin1, cabin2, cabin3)
+
+        println(WeatherChecker.sort(cabins, "rain"))
+
+        assertEquals(cabin2, WeatherChecker.sort(cabins, "rain").get(0))
+    }
+
+    @Test
+    fun sortTestEqualWind() {
+        val cabin1 = Cabin(
+            1, "Skihytta", null, null, 17.0,
+            9.0, 50.0, 18.0, 9.0,
+            null, null, null, null, null,
+            null, null, null, null, null,
+            null
+        )
+
+        val cabin2 = Cabin(
+            1, "Skihytta", null, null, 19.0,
+            9.0, 40.0, 19.0, 12.0,
+            null, null, null, null, null,
+            null, null, null, null, null,
+            null
+        )
+
+        val cabin3 = Cabin(
+            1, "Skihytta", null, null, 18.0,
+            9.0, 60.0, 15.0, 11.0,
+            null, null, null, null, null,
+            null, null, null, null, null,
+            null
+        )
+
+        val cabins = listOf(cabin1, cabin2, cabin3)
+
+        println(WeatherChecker.sort(cabins, "wind"))
+
+        assertEquals(cabin2, WeatherChecker.sort(cabins, "wind").get(0))
     }
 }
