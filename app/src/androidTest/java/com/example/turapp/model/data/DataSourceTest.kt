3161c1c3c1e414e1a72586aa28170868d189b098
sondm <Tumejorpesadillacharmander95!>
2022-05-19 16:32:18 +0200

    Updated DataSourceTest.kt and removed warnings

--- app/src/androidTest/java/com/example/turapp/model/data/DataSourceTest.kt
+++ app/src/androidTest/java/com/example/turapp/model/data/DataSourceTest.kt
@@ -1,22 +1,16 @@
 package com.example.turapp.model.data
 
-import android.content.Context
-import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.turapp.model.interfaces.CabinApi
-import com.example.turapp.model.interfaces.RetrofitHelper
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import retrofit2.Retrofit
 
 @RunWith(AndroidJUnit4::class)
 class DataSourceTest {
 
-    lateinit var dataSource: DataSource
+    private lateinit var dataSource: DataSource
 
     @Before
     fun setup() {
@@ -32,7 +26,7 @@ class DataSourceTest {
     @Test
     fun checkFirstCabin() = runBlocking {
         val cabins = dataSource.fetchCabins()
-        val name = cabins.get(0).name
+        val name = cabins[0].name
         assertEquals("Gamle Storlihytta", name)
 
     }
